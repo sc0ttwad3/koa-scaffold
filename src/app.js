a35f829d759a22f0d693cbceb24bf97d7ea69b82
Scott Wade <sc0ttwad3@gmail.com>
2018-02-10 12:15:00 -0500

    Add index.html and first try on server/app slit with koa-router

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk')
 const Koa = require('koa')
 const Router = require('koa-router')
 const joi = require('joi')
@@ -5,5 +6,36 @@ const validate = require('koa-joi-validate')
 
 const app = new Koa();
 const router = new Router();
+
+// log each request to the console
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// set permissive CORS header
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  return next;
+})
+
+app.use(router.routes())
+   .use(router.allowedMethods());
+
+app.on('error', e => {
+  console.error(chalk.bold.red('Server Error', e));
+});
+
+/*
+router.get('/', async (ctx, next) => {
+  const {term, ofsset} = ctx.request.query;
+  // ctx available
+  // ctx.body = await Some_Function()
+})
+*/
+
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const server = require('./app')
 
 /* Bootstrap Koa server */
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '4000';
 
 async function bootstrap() {
   // app established to function as HTTP server
