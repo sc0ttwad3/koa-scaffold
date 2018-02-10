d7d8fb173dc1058075bf66160b2b9278681033f3
Scott Wade <sc0ttwad3@gmail.com>
2018-02-10 11:44:03 -0500

    split up server for HTTP from the Koa application that will serve as the server

--- src/server.js
+++ src/server.js
@@ -1,21 +1,13 @@
 const chalk = require('chalk');
 
+const server = require('./app')
 
-
-/*
-const debug = require('debug')('express-scaffold:server');
-const http = require('http');
-const app = require('./app');
-
- *  Boostrap Express server
+/* Bootstrap Koa server */
 const port = process.env.PORT || '3000';
 
 async function bootstrap() {
-  const server = await http.createServer(app);
-  //server.on('error', onError);
-  //server.on('listening', onListening);
-  await server.listen(port, () => console.log(chalk.bold.blue(`Express server listening on ${port} ...`)));
+  // app established to function as HTTP server
+  await server.listen(port, () => console.log(chalk.bold.blue(`Koa server listening on ${port} ...`)));
 }
 
 bootstrap();
-*/
